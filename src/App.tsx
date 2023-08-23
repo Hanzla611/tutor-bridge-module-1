af7a9d1972d06ae5b1940fc9f5c41a54e038c7f0
abu-hanzla <hanzla611@gmail.com>
2023-08-23 15:17:33 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import "./App.css";
 import { useState } from "react";
-
 import Footer from "./components/Footer/index.tsx";
 import Header from "./components/Header/Header";
-import Homepage from "./pages/homepage/index.tsx";
-import { useRef } from 'react';
+import { useRef } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppRoutes from "./routes.jsx";
+import Homepage from "./pages/homepage";
+import Register from "./pages/register/index";
 
 function App() {
   const [night, setNight] = useState(false);
   const contactRef = useRef(null);
 
   const scrollToContact = () => {
-    contactRef.current.scrollIntoView({ behavior: 'smooth' });
+    contactRef.current.scrollIntoView({ behavior: "smooth" });
   };
   return (
     <div className={night ? "night-mode" : ""}>
-      <Header
-        night={night}
+      <Router>
+        <Header
+          night={night}
+          setNight={setNight}
+          scrollToContact={scrollToContact}
+        />
+          <Routes>
+          <Route exact path="/" element={<Homepage night={night}
         setNight={setNight}
-        scrollToContact={scrollToContact}
-      />
-      <Homepage
-        night={night}
-        setNight={setNight}
-        contactRef={contactRef} 
-      />
-      <Footer />
+        contactRef={contactRef}  />} />
+      <Route path="/signup" element={<Register />} />
+          </Routes>
+     
+        <Footer />
+      </Router>
+      {/* <Register /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
